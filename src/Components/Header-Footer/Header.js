e5cc3ecb92380cbde12a1ddb490592f4e2b7064f
Harsh Panwar <20001003044harsh@dcrustm.org>
2024-05-08 22:10:35 +0530

    Middle Body setup

--- src/Components/Header-Footer/Header.js
+++ src/Components/Header-Footer/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Logo from "../../Assests/Images/Excited_Girl.png";
+import Logo from "../../Assests/Images/logo.svg";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
--- src/Components/HomePage/Home.jsx
+++ src/Components/HomePage/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../Header-Footer/Header.js";
 import Footer from "../Header-Footer/Footer.js";
+import Body from "./Body.jsx";
 
 const Home = () => {
   return (
     <>
       <Header />
-
+<Body/>
       <Footer />
     </>
   );
